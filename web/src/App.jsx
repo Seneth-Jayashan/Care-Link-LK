7de5173f59e177f0613cf4337e301c3a3f0f9004
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-10-17 00:45:37 +0530

    Navbar and Footer

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
 
       </Routes>
+      <Footer />
     </div>
   )
 }
