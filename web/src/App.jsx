d00bd38a6cc518e04c9261f84c0849f7739e4f96
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-10-17 01:37:47 +0530

    updated

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,6 +4,9 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import AddHospital from './pages/hospitaladmin/AddHospital'
+import Aboutus from './pages/Aboutus'
+
 
 function App() {
   return (
@@ -13,7 +16,9 @@ function App() {
       
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
+        <Route path="/add-hospital" element={<AddHospital />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        
       </Routes>
       <Footer />
     </div>
